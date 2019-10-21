a48017656560ed1a4322e2ca9a93cb65dda4f233
PoojaBhatS <pooja.santhapadavu@stv.tv>
2019-10-21 14:27:44 +0100

    Initial Commit

---
+++ Pulse_Sample_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pulse_Sample_App
+//
+//  Created by Pooja Santhapadavu on 21/10/2019.
+//  Copyright © 2019 Pooja Santhapadavu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+override func viewDidLoad() {
+super.viewDidLoad()
+// Do any additional setup after loading the view.
+}
+
+
+}
+
---
+++ Pulse_Sample_AppTests/Pulse_Sample_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pulse_Sample_AppTests.swift
+//  Pulse_Sample_AppTests
+//
+//  Created by Pooja Santhapadavu on 21/10/2019.
+//  Copyright © 2019 Pooja Santhapadavu. All rights reserved.
+//
+
+import XCTest
+@testable import Pulse_Sample_App
+
+class Pulse_Sample_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
